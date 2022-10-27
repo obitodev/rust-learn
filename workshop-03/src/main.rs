47dfb2e4b5d1841c7d011a07987dba9d392e70c2
obitodev <anhhaiquynhon123@gmail.com>
2022-10-27 21:22:04 +0700

    fix ex02

--- workshop-03/src/main.rs
+++ workshop-03/src/main.rs
@@ -4,16 +4,38 @@
 // + Sửa code liên quan tới vấn đề generic type (thay đổi ở định nghĩa struct)
 ///////////////////////////////////////////
 
-#[derive(Debug)]
-struct Point<T, X> {
-    x: T,
-    y: X,
-}
+// #[derive(Debug)]
+// struct Point<T, X> {
+//     x: T,
+//     y: X,
+// }
 
-fn main() {
-    // DON'T modify this code.
-    let p = Point{x: 5, y : "hello".to_string()};
+// fn main() {
+//     // DON'T modify this code.
+//     let p = Point{x: 5, y : "hello".to_string()};
 
-    println!("Success!");
-    println!("{:?}", p);
-}
+//     println!("Success!");
+//     println!("{:?}", p);
+// }
+
+///////////////////////////////////////////
+// BAI 2
+// Yêu cầu :
+// + Implement hàm sum dưới đây, sao cho việc kiểm tra assert_eq đúng 
+///////////////////////////////////////////
+
+// use std::ops::Add;
+
+
+// // Implement the generic function below.
+// fn sum<T:Add<Output = T>>(x:T, y:T) -> T{
+//     x + y
+// }
+
+// fn main() {
+//     assert_eq!(5, sum(2i8, 3i8));
+//     assert_eq!(50, sum(20, 30));
+//     assert_eq!(2.46, sum(1.23, 1.23));
+
+//     println!("Success!");
+// }
