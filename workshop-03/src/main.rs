118d5e2e2cde0a4ce8e07394e051e5e0173e9566
obitodev <anhhaiquynhon123@gmail.com>
2022-10-27 21:33:13 +0700

    fix ex03

--- workshop-03/src/main.rs
+++ workshop-03/src/main.rs
@@ -39,3 +39,31 @@
 
 //     println!("Success!");
 // }
+
+
+///////////////////////////////////////////
+// BAI 3
+// Yêu cầu :
+// + Sửa lại code sao cho compile cho đúng 
+///////////////////////////////////////////
+
+
+fn main() {
+    let a = A {p: Some("p".to_string())};
+    a.a();
+}
+
+struct A {
+    p: Option<String>
+}
+
+
+impl A {
+    fn a(self) -> Self {
+        Self::b(&self.p.clone().unwrap());
+        self
+    }
+    fn b(b: &str) {
+        print!("b: {}", b)
+    }
+}
