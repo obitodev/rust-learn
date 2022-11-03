3a36a052dc0209fdff57f829e9a239ee890017d4
obitodev <anhhaiquynhon123@gmail.com>
2022-11-03 17:00:36 +0700

    fix ex03

--- workshop-04/src/main.rs
+++ workshop-04/src/main.rs
@@ -63,3 +63,34 @@
 //         );
 //     }
 // }
+
+/////////////////////////////////////
+// Bài 3
+// Implement trait
+// Thực hiên cargo test
+/////////////////////////////////////
+
+
+
+// trait AppendBar {
+//     fn append_bar(self) -> Self;
+// }
+
+// impl AppendBar for Vec<String> {
+//     fn append_bar(mut self) -> Self {
+//         self.push("Bar".to_string());
+//         self
+//     }
+// }
+// //TODO: Add your code here
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn is_vec_pop_eq_bar() {
+//         let mut foo = vec![String::from("Foo")].append_bar();
+//         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+//         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+//     }
+// }
