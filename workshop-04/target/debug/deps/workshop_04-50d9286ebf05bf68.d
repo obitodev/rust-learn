1a2062463a1152bf2038ab565c3a2fd64ecf3012
obitodev <anhhaiquynhon123@gmail.com>
2022-11-03 16:49:16 +0700

    fix ex02

---
+++ workshop-04/target/debug/deps/workshop_04-50d9286ebf05bf68.d
@@ -0,0 +1,5 @@
+/home/ns1559/working-space/blockchain-dev/blockchain-rust/rust-learn/workshop-04/target/debug/deps/workshop_04-50d9286ebf05bf68: src/main.rs
+
+/home/ns1559/working-space/blockchain-dev/blockchain-rust/rust-learn/workshop-04/target/debug/deps/workshop_04-50d9286ebf05bf68.d: src/main.rs
+
+src/main.rs:
